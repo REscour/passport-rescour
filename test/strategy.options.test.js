623fdffc297096531a30d08c47ef07ac465c1efe
Spencer Applegate <spencer@spencerapplegate.com>
2014-10-20 15:26:57 -0400

    Cleaned up tests

--- test/strategy.options.test.js
+++ test/strategy.options.test.js
@@ -1,31 +1,28 @@
 /* global describe, it, expect, before */
 /* jshint expr: true */
 
-var FacebookStrategy = require('../lib/strategy');
-
+var REscourStrategy = require('../lib/strategy');
 
 describe('Strategy#userProfile', function() {
-  
+
   describe('with default URL and proof enabled', function() {
-    var strategy = new FacebookStrategy({
+    var strategy = new REscourStrategy({
         clientID: 'ABC123',
-        clientSecret: 'secret',
-        enableProof: true
+        clientSecret: 'secret'
       },
       function() {});
-  
+
     // mock
     strategy._oauth2.get = function(url, accessToken, callback) {
-      if (url != 'https://graph.facebook.com/me?appsecret_proof=e941110e3d2bfe82621f0e3e1434730d7305d106c5f68c87165d0b27a4611a4a') { return callback(new Error('incorrect url argument')); }
       if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
-    
-      var body = '{"id":"500308595","name":"Jared Hanson","first_name":"Jared","last_name":"Hanson","link":"http:\\/\\/www.facebook.com\\/jaredhanson","username":"jaredhanson","gender":"male","email":"jaredhanson\\u0040example.com"}';
+
+      var body = '{"id":"500308595","name":"Spencer Applegate","first_name":"Spencer","last_name":"Applegate"}';
       callback(null, body, undefined);
     };
-    
+
     describe('loading profile', function() {
       var profile;
-    
+
       before(function(done) {
         strategy.userProfile('token', function(err, p) {
           if (err) { return done(err); }
@@ -33,35 +30,33 @@ describe('Strategy#userProfile', function() {
           done();
         });
       });
-    
+
       it('should parse profile', function() {
-        expect(profile.provider).to.equal('facebook');
+        expect(profile.provider).to.equal('rescour');
         expect(profile.id).to.equal('500308595');
-        expect(profile.username).to.equal('jaredhanson');
+        expect(profile.username).to.equal('spencerapplegate');
       });
     });
   });
-    
+
   describe('with profile URL option', function() {
-    var strategy = new FacebookStrategy({
+    var strategy = new REscourStrategy({
         clientID: 'ABC123',
-        clientSecret: 'secret',
-        profileURL: 'https://graph.facebook.com/me?fields=id,username'
+        clientSecret: 'secret'
       },
       function() {});
-  
+
     // mock
     strategy._oauth2.get = function(url, accessToken, callback) {
-      if (url != 'https://graph.facebook.com/me?fields=id,username') { return callback(new Error('incorrect url argument')); }
       if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
-    
-      var body = '{"id":"500308595","name":"Jared Hanson","first_name":"Jared","last_name":"Hanson","link":"http:\\/\\/www.facebook.com\\/jaredhanson","username":"jaredhanson","gender":"male","email":"jaredhanson\\u0040example.com"}';
+
+      var body = '{"id":"500308595","name":"Spencer Applegate","first_name":"Spencer","last_name":"Applegate"}';
       callback(null, body, undefined);
     };
-    
+
     describe('loading profile', function() {
       var profile;
-    
+
       before(function(done) {
         strategy.userProfile('token', function(err, p) {
           if (err) { return done(err); }
@@ -69,36 +64,33 @@ describe('Strategy#userProfile', function() {
           done();
         });
       });
-    
+
       it('should parse profile', function() {
-        expect(profile.provider).to.equal('facebook');
+        expect(profile.provider).to.equal('rescour');
         expect(profile.id).to.equal('500308595');
-        expect(profile.username).to.equal('jaredhanson');
+        expect(profile.username).to.equal('spencerapplegate');
       });
     });
   });
-  
+
   describe('with profile URL option and proof enabled', function() {
-    var strategy = new FacebookStrategy({
+    var strategy = new REscourStrategy({
         clientID: 'ABC123',
-        clientSecret: 'secret',
-        profileURL: 'https://graph.facebook.com/me?fields=id,username',
-        enableProof: true
+        clientSecret: 'secret'
       },
       function() {});
-  
+
     // mock
     strategy._oauth2.get = function(url, accessToken, callback) {
-      if (url != 'https://graph.facebook.com/me?fields=id,username&appsecret_proof=e941110e3d2bfe82621f0e3e1434730d7305d106c5f68c87165d0b27a4611a4a') { return callback(new Error('incorrect url argument')); }
       if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
-    
-      var body = '{"id":"500308595","name":"Jared Hanson","first_name":"Jared","last_name":"Hanson","link":"http:\\/\\/www.facebook.com\\/jaredhanson","username":"jaredhanson","gender":"male","email":"jaredhanson\\u0040example.com"}';
+
+      var body = '{"id":"500308595","name":"Spencer Applegate","first_name":"Spencer","last_name":"Applegate"}';
       callback(null, body, undefined);
     };
-    
+
     describe('loading profile', function() {
       var profile;
-    
+
       before(function(done) {
         strategy.userProfile('token', function(err, p) {
           if (err) { return done(err); }
@@ -106,67 +98,29 @@ describe('Strategy#userProfile', function() {
           done();
         });
       });
-    
+
       it('should parse profile', function() {
-        expect(profile.provider).to.equal('facebook');
+        expect(profile.provider).to.equal('rescour');
         expect(profile.id).to.equal('500308595');
-        expect(profile.username).to.equal('jaredhanson');
+        expect(profile.username).to.equal('spencerapplegate');
       });
     });
   });
-  
+
   describe('with profile fields mapped from portable contacts schema', function() {
-    var strategy = new FacebookStrategy({
+    var strategy = new REscourStrategy({
         clientID: 'ABC123',
-        clientSecret: 'secret',
-        profileFields: ['id', 'username', 'displayName', 'name', 'gender', 'profileUrl', 'emails', 'photos']
+        clientSecret: 'secret'
       },
       function() {});
-  
+
     // mock
     strategy._oauth2.get = function(url, accessToken, callback) {
-      if (url != 'https://graph.facebook.com/me?fields=id,username,name,last_name,first_name,middle_name,gender,link,email,picture') { return callback(new Error('incorrect url argument')); }
       if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
-    
-      var body = '{"id":"500308595","name":"Jared Hanson","first_name":"Jared","last_name":"Hanson","link":"http:\\/\\/www.facebook.com\\/jaredhanson","username":"jaredhanson","gender":"male","email":"jaredhanson\\u0040example.com"}';
+
+      var body = '{"id":"500308595","name":"Spencer Applegate","first_name":"Spencer","last_name":"Applegate"}';
       callback(null, body, undefined);
     };
-    
-    describe('loading profile', function() {
-      var profile;
-    
-      before(function(done) {
-        strategy.userProfile('token', function(err, p) {
-          if (err) { return done(err); }
-          profile = p;
-          done();
-        });
-      });
-    
-      it('should parse profile', function() {
-        expect(profile.provider).to.equal('facebook');
-        expect(profile.id).to.equal('500308595');
-        expect(profile.username).to.equal('jaredhanson');
-      });
-    });
-  });
-
-  describe('with profile fields not mapped from portable contacts schema', function() {
-    var strategy = new FacebookStrategy({
-        clientID: 'ABC123',
-        clientSecret: 'secret',
-        profileFields: ['id', 'username', 'displayName', 'name', 'gender', 'profileUrl', 'emails', 'photos', 'updated_time']
-      },
-      function() {});
-
-      // mock
-      strategy._oauth2.get = function(url, accessToken, callback) {
-        if (url != 'https://graph.facebook.com/me?fields=id,username,name,last_name,first_name,middle_name,gender,link,email,picture,updated_time') { return callback(new Error('incorrect url argument')); }
-        if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
-
-        var body = '{"id":"500308595","name":"Jared Hanson","first_name":"Jared","last_name":"Hanson","link":"http:\\/\\/www.facebook.com\\/jaredhanson","username":"jaredhanson","gender":"male","email":"jaredhanson\\u0040example.com", "updated_time": "2013-11-02T18:33:09+0000"}';
-        callback(null, body, undefined);
-      }
 
     describe('loading profile', function() {
       var profile;
@@ -180,52 +134,11 @@ describe('Strategy#userProfile', function() {
       });
 
       it('should parse profile', function() {
-        expect(profile.provider).to.equal('facebook');
+        expect(profile.provider).to.equal('rescour');
         expect(profile.id).to.equal('500308595');
-        expect(profile.username).to.equal('jaredhanson');
+        expect(profile.username).to.equal('spencerapplegate');
       });
+    });
+  });
 
-      it('should have additional fields in profile._json', function() {
-        expect(profile._json.updated_time).to.equal('2013-11-02T18:33:09+0000');
-      });
-    });
-  });
-  
-  describe('with profile fields mapped from portable contacts schema and proof enabled', function() {
-    var strategy = new FacebookStrategy({
-        clientID: 'ABC123',
-        clientSecret: 'secret',
-        profileFields: ['id', 'username', 'displayName', 'name', 'gender', 'profileUrl', 'emails', 'photos'],
-        enableProof: true
-      },
-      function() {});
-  
-    // mock
-    strategy._oauth2.get = function(url, accessToken, callback) {
-      if (url != 'https://graph.facebook.com/me?appsecret_proof=e941110e3d2bfe82621f0e3e1434730d7305d106c5f68c87165d0b27a4611a4a&fields=id,username,name,last_name,first_name,middle_name,gender,link,email,picture') { return callback(new Error('incorrect url argument')); }
-      if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
-    
-      var body = '{"id":"500308595","name":"Jared Hanson","first_name":"Jared","last_name":"Hanson","link":"http:\\/\\/www.facebook.com\\/jaredhanson","username":"jaredhanson","gender":"male","email":"jaredhanson\\u0040example.com"}';
-      callback(null, body, undefined);
-    };
-    
-    describe('loading profile', function() {
-      var profile;
-    
-      before(function(done) {
-        strategy.userProfile('token', function(err, p) {
-          if (err) { return done(err); }
-          profile = p;
-          done();
-        });
-      });
-    
-      it('should parse profile', function() {
-        expect(profile.provider).to.equal('facebook');
-        expect(profile.id).to.equal('500308595');
-        expect(profile.username).to.equal('jaredhanson');
-      });
-    });
-  });
-  
 });
